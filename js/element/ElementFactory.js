14d6b2509705645bd1fd0abafcf9d1890a6ab92c
barsdeveloper <barsdeveloper@gmail.com>
2023-09-20 18:53:05 +0200

    Fix type 1

--- js/element/ElementFactory.js
+++ js/element/ElementFactory.js
@@ -1,11 +1,11 @@
 export default class ElementFactory {
 
-    /** @type {Map<String, ElementConstructor>} */
+    /** @type {Map<String, Constructor<IElement>>} */
     static #elementConstructors = new Map()
 
     /**
      * @param {String} tagName
-     * @param {ElementConstructor} entityConstructor
+     * @param {Constructor<IElement>} entityConstructor
      */
     static registerElement(tagName, entityConstructor) {
         ElementFactory.#elementConstructors.set(tagName, entityConstructor)
