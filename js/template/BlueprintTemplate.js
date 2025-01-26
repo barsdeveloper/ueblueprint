c5f57dd3ef20dc1fb7b2ddb8ed7dc45342f1baad
barsdeveloper <barsdeveloper@gmail.com>
2025-01-26 14:53:02 +0200

    Remove dependency and minot improvement

--- js/template/BlueprintTemplate.js
+++ js/template/BlueprintTemplate.js
@@ -169,8 +169,8 @@ export default class BlueprintTemplate extends ITemplate {
                     License: MIT<br />
 
                     <a target="_blank" href="https://github.com/barsdeveloper/ueblueprint">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="#e3b341" style="vertical-align: bottom">
-                            <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path>
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="#e3b341" style="vertical-align: bottom">
+                            <path d="M 12 0.587 L 16 8 L 24 9 L 18 15 L 20 24 L 12 19 L 4 24 L 6 15 L 0 9 L 8 8 Z M 0 0"></path>
                         </svg>
                         https://github.com/barsdeveloper/ueblueprint
                     </a>
